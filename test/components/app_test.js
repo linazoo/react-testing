b39ab96cf7c8c1974c1b594b8aabfe49922c2601
lina zoo <linazooper@gmail.com>
2018-01-01 14:26:59 -0600

    wrote first mocha passing test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -7,8 +7,19 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // use 'describe' to group tgether similar tests 
-describe('App') 
-//use 'it' to test a single attribute of a target - something specific about App
-it('shows the correct text') 
+describe('App', () => {
+  
+  
+  //use 'it' to test a single attribute of a target - something specific about App
+  it('shows the correct text', () => {
 
-expect 
+    // create an instance of App
+    const component = renderComponent(App);
+    
+    // use 'expect' to make an 'assertion' about the target
+    expect(component).to.contain('React simple starter'); 
+
+  }); 
+  
+
+}); 
