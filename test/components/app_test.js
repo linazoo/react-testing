74c4776a0f7a45c2dc23401f24ad56c8a4f02d7e
lina zoo <linazooper@gmail.com>
2018-01-01 14:05:04 -0600

    started writing first test for ap

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,14 @@
-import { renderComponent , expect } from '../test_helper';
+// what is the behaviour about this component i want to test? 
+// what do i care about someone not breaking? 
+
+// what are we trying to test?? that the app renders the div with the text 
+
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// use 'describe' to group tgether similar tests 
+describe('App') 
+//use 'it' to test a single attribute of a target - something specific about App
+it('shows the correct text') 
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+expect 
