5a6816b02966f1efb2d8cce71c6bd8982100d064
lina zoo <linazooper@gmail.com>
2018-01-02 20:55:29 -0600

    put together first assertion for commentBox where we expected the textarea to exist and how the component is our jquery object yay

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,13 +3,13 @@ import CommentBox from '../../src/components/comment_box';
 
 describe ('CommentBox', () => {
   it ('has a text area', () => {
-
+    const component = renderComponent(CommentBox);
+    expect(component.find('textarea')).to.exist;
   });
 
   it ('has a button', () => {
 
   });
   
-
   expect 
 });
