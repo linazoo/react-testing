0664632597266a6b97f8e522dfbdda31ef90c867
linazoo <linazooper@gmail.com>
2018-01-24 21:17:56 -0600

    Specs failure after code change and how to fix it

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,3 +1,10 @@
+import { SAVE_COMMENT } from '../actions/types';
+
 export default function(state = [], action) {
+  switch(action.type) {
+    case SAVE_COMMENT:
+    // same thing as writing return state.concat([action.payload]); takes the exisitng array and concats another array containing action.ppayload
+    return [ ...state, action.payload ];
+  }
   return state;
 }
